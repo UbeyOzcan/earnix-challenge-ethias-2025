dc92eb2bd1d2e996f23082c154140d9e7b781880
OZCAN Ubeydullah <P12937@ethias.be>
2025-10-30 10:43:25 +0100

    draft

--- main.py
+++ main.py
@@ -1,28 +1,50 @@
-from src import DataFactor
+from src import Factory
 import numpy as np
-from scipy import stats
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
+
+def clean(df: pd.DataFrame) -> pd.DataFrame:
+
+    df = DF.replace_empty_cost(dataframe=df)
+
+    df = DF.replace_mode_mean(dataframe=df)
+    df.to_csv('db/clean_train.csv', sep=";", index=False)
+    return df
 
 
-DF = DataFactor(db_path='db')
-df = DF.getriskdb()
+def sev_dist(df: pd.DataFrame, name:str) -> None:
 
-df = DF.replace_empty_cost(dataframe=df)
+    df['M_ClaimCount'] = df['M_ClaimCount'].astype(float)
+    df['ln_cost'] = np.where(df['Totalcost'] > 0, np.log(df['Totalcost']), 0)
+    df = df[df['ln_cost'] > 0]
+    dist_severity =  sns.displot(df, x='ln_cost', kde=True, rug=False)
+    plt.savefig(f'dist_ln_cost_{name}.png')
+    return None
 
-df = DF.replace_mode_mean(dataframe=df)
-df.to_csv('db/clean_train.csv', sep=";", index=False)
-exit()
+def prediction_dist(df:pd.DataFrame) -> None:
+    df = df[df['Actual'] > 0]
+    actual = [np.where(df['Actual'] > 0, np.log(df['Actual']), 0)]
+    pred = [np.where(df['Pred'] > 0, np.log(df['Pred']), 0)]
 
-df['Totalcost'] = df['Totalcost'].fillna(0)
-df['M_ClaimCount'] = df['M_ClaimCount'].astype(float)
-df['sev'] =  np.where(df['M_ClaimCount'] > 0, df['Totalcost']/df['M_ClaimCount'], 0)
-totalcost = list(np.log(df[(df['Totalcost'] > 0)]['Totalcost']))
-severity = list(np.log(df[(df['sev'] > 0)]['sev']))
+    actual_pred  = [a/b for a,b in zip(actual,pred)]
 
-dist_total_cost = sns.displot(totalcost, kde=True, rug=False)
-plt.savefig('dist_total_cost.png')
+    sns.displot(actual, kde=True, rug=False)
+    plt.savefig(f'dist_actual.png')
+    sns.displot(pred, kde=True, rug=False)
+    plt.savefig(f'dist_predicted.png')
+    sns.displot(actual_pred, kde=True, rug=False)
+    plt.savefig(f'dist_actual_predicted.png')
+    return None
 
-
-dist_severity =  sns.displot(severity, kde=True, rug=False)
-plt.savefig('dist_severity.png')
+do_clean = False
+do_dist = False
+do_dist_pred = False
+calc_mse = True
+if __name__ == '__main__':
+    DF = Factory(db_path='db')
+    if calc_mse:
+        df = DF.getriskdb()
+        df_dict = DF.dist_separator(dataframe=df, sep=600)
+        mse = DF.mse_calc(df=df_dict['dist_1'])
+        print(mse)
--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import numpy as np
+from sklearn.metrics import mean_squared_error
 
-
-class DataFactor:
+class Factory:
     def __init__(self, db_path: str):
         self.db_path = db_path
 
     def getriskdb(self) -> pd.DataFrame:
         return pd.read_csv(f'{self.db_path}/Train Data.csv', sep=";", decimal=',')
+
+    def getriskdb_clean(self) -> pd.DataFrame:
+        return pd.read_csv(f'{self.db_path}/clean_train.csv', sep=";", decimal=',')
 
     def getgeodb(self) -> pd.DataFrame:
         return pd.read_csv(f'{self.db_path}/Geo Enrichment.csv')
@@ -27,3 +30,38 @@ class DataFactor:
                 dataframe[i] = dataframe[i].fillna(dataframe[i].mean())
 
         return dataframe
+
+    def dist_separator(self, dataframe:pd.DataFrame, sep:int) -> dict:
+
+        dataframe['M_ClaimCount'] = dataframe['M_ClaimCount'].astype(float)
+        dataframe = dataframe[dataframe['M_ClaimCount'] < 2]
+        n_claims = dataframe['M_ClaimCount'].sum()
+        df_dist_1 = dataframe.copy()
+        df_dist_2 = dataframe.copy()
+        del dataframe
+
+        df_dist_1['Totalcost'] = np.where(df_dist_1['Totalcost'] <= sep, df_dist_1['Totalcost'] , 0)
+        df_dist_1['M_ClaimCount'] = np.where((df_dist_1['Totalcost'] <= sep) & (df_dist_1['Totalcost'] > 0), 1, 0)
+
+        df_dist_2['Totalcost'] = np.where(df_dist_2['Totalcost'] > sep, df_dist_2['Totalcost'], 0)
+        df_dist_2['M_ClaimCount'] = np.where(df_dist_2['Totalcost'] > sep, 1, 0)
+        print(df_dist_1['M_ClaimCount'].sum() + df_dist_2['M_ClaimCount'].sum() == int(n_claims))
+        #df_dist_1.to_csv(f'{self.db_path}/Train_Lower.csv', sep = ";", decimal = ",", index=False)
+        #df_dist_2.to_csv(f'{self.db_path}/Train_Upper.csv', sep = ";", decimal = ",", index=False)
+
+        df_dict = {'dist_1': df_dist_1, 'dist_2': df_dist_2}
+        return df_dict
+
+    def get_pred_obs(self) -> pd.DataFrame:
+        return pd.read_csv(f'{self.db_path}/DataPredictions_GBM_Tweedie_full.csv', sep=",", decimal='.')
+
+    def mse_calc(self, df: pd.DataFrame) -> list:
+
+        df.insert(len(df.columns), "Pred", 278.071)
+        df.insert(len(df.columns), "Pred_1", 0)
+        actual = [df['Totalcost']]
+        pred = [df['Pred']]
+        pred_1 = [df['Pred_1']]
+        mse = mean_squared_error(actual, pred)
+        mse_1 = mean_squared_error(actual, pred_1)
+        return [mse, mse_1]
